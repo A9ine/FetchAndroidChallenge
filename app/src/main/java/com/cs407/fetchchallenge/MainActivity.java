9032e74017eba1cf4b30103c565723dd3125bf5d
Andrew Zhang <andrewzhang2323@gmail.com>
2024-03-19 10:51:37 +0800

    Initial commit

---
+++ app/src/main/java/com/cs407/fetchchallenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cs407.fetchchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aactivity_main);
+    }
+}
